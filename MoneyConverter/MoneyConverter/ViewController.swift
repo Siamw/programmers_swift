443ab5177c2b142b3ec2ecd980019656530272d2
lecture <lecture@macs-MacBook-Pro.local>
2018-04-15 18:47:04 +0900

    insert function code

--- MoneyConverter/MoneyConverter/ViewController.swift
+++ MoneyConverter/MoneyConverter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currencySegment: UISegmentedControl!
+    @IBOutlet weak var sourceMoneyField: UITextField!
+    @IBOutlet weak var targetMoneyLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertMoney(_ sender: Any) {
+        let ratio : Double
+        switch currencySegment.selectedSegmentIndex{
+        case 0:
+            ratio = 0.00085
+        case 1:
+            ratio = 1178.5
+        default:
+            ratio = 1.0
+        }
+        let targetMoneyString:String
+        if let sourceMoney = Double(sourceMoneyField.text!)
+        {
+            targetMoneyString="\(sourceMoney * ratio)"
+        }
+        else {targetMoneyString = "Error"}
+        
+        targetMoneyLable.text=targetMoneyString
+    }
+    
 }
 
--- MoneyConverter/README.md
+++ MoneyConverter/README.md
@@ -1,2 +1,2 @@
-# Single View App
+# Single View App 만들기
 [programmer swift 입문 강의](https://programmers.co.kr/learn/courses/4)
